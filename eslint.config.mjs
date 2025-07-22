4d46b3ae324e95d25e75e863929c34c56124d835
giopirveli <giorgipirveli000@gmail.com>
2025-07-22 14:37:40 +0400

    Moved project to root and cleaned structure

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
+import js from "@eslint/js"; // ეს გაკლდა – აუცილებელია
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  js.configs.recommended, // ახლა იმუშავებს
+
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/dist/**",
+      "**/out/**",
+      "**/coverage/**",
+      "**/public/**",
+    ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+];
