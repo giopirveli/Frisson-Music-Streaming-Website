4627f394cb54861ce5703b13577eab4ca0d8fcfd
giopirveli <giorgipirveli000@gmail.com>
2025-08-01 20:00:17 +0400

    fix/header and sidebar distence

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,17 +6,15 @@ import Header from "@/components/Header/Header";
 export default function Home() {
   return (
     <div className={styles.page}>
-
       <Sidebar></Sidebar>
+
       <div className={styles.container}>
         <Header></Header>
         <main className={styles.main}>
-
+          
         </main>
       </div>
-      <footer className={styles.footer}>
 
-      </footer>
     </div>
   );
 }
