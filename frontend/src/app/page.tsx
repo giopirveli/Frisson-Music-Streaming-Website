009d070fa460154443340f698de5a4a4103f8c66
DanielIsayan777 <Dennyinside30@gmail.com>
2025-08-05 19:11:43 +0400

    Create NewsComponent

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@
 import styles from "./page.module.scss";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Header from "@/components/Header/Header";
+import NewsComponent from "@/components/NewsComponent/NewsComponent";
 
 export default function Home() {
   return (
@@ -11,7 +12,11 @@ export default function Home() {
       <div className={styles.container}>
         <Header></Header>
         <main className={styles.main}>
-          
+        <NewsComponent
+         imageUrl={"string"}
+         title={"New Hit"}
+         plays={543534953}
+        ></NewsComponent>
         </main>
       </div>
 
