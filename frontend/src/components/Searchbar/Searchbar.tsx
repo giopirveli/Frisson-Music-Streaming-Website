0fda038b70030bc45aff521f399adfbe8a45cefe
PARADOXnews <124525161+PARADOXnews@users.noreply.github.com>
2025-08-09 15:48:31 +0400

    feat/searchbar
    fixed sidebar and header

---
+++ frontend/src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,39 @@
+// Searchbar.tsx
+import styles from "./Searchbar.module.scss";
+
+type Props = {
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; /* we need to replace void using the function name */
+  placeholder?: string;
+  iconName?: string; // example: "search.svg"
+};
+
+
+
+
+export default function Searchbar({
+  value,
+  onChange,
+  placeholder = "Search...",
+  iconName,
+}: Props) {
+  return (
+    <div className={styles.wrapper}>
+      {iconName && (
+        <img
+          src={`/${iconName}`}
+          alt="icon"
+          className={styles.icon}
+          draggable={false}
+        />
+      )}
+      <input
+        type="text"
+        className={styles.searchbar}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
