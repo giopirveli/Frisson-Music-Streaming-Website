6576e847bd85a779bc6a7ae4de54838e4bab8505
giopirveli <giorgipirveli000@gmail.com>
2025-08-15 00:11:32 +0400

    clear up code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,9 +3,6 @@ import styles from "./page.module.scss";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Header from "@/components/Header/Header";
 import Player from "@/components/Player/player";
-import ArtistCard from "@/components/ArtistCard/ArtistCard";
-import AlbumCard from "@/components/AlbumCard/AlbumCard";
-import MusicCard from "@/components/MusicCard/MusicCard";
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -13,9 +10,6 @@ export default function Home() {
       <div className={styles.container}>
         <Header></Header>
         <main className={styles.main}>
-          <ArtistCard  title="eee"  imageUrl="/Images/ArtistCard/ArtistPhoto.jpg"/>
-                    <AlbumCard title="eee" artist="eeee" imageUrl="/Images/AlbumCard/AlbumPhoto.jpg"/>
-          <MusicCard title="eee" artist="eeee" imageUrl="/Images/MusicCard/MusicCardPhoto.jpg" />
         </main>
       </div>
       <Player />
