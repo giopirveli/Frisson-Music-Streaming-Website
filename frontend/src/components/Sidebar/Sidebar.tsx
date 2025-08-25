7f4ceb641e534d80c1fd7b13deea7a5b8c1ea388
DanielIsayan777 <Dennyinside30@gmail.com>
2025-08-25 15:56:17 +0400

    Album Page is done

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -45,7 +45,7 @@ export default function Sidebar() {
             <div className={styles.discoverMenu}>
                <h4>discover</h4>
                <NavItem href="/auth/ArtistPage" icon="/icons/Sidebar/artist.svg" label="artist" />
-               <NavItem href="/albums" icon="/icons/Sidebar/album.svg" label="album" />
+               <NavItem href="/auth/AlbumPage" icon="/icons/Sidebar/album.svg" label="album" />
             </div>
          </nav>
       </aside>
