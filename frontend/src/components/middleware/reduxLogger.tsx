b0b820299f9c31f862dce64161f3f881a31440b6
PARADOXnews <124525161+PARADOXnews@users.noreply.github.com>
2025-09-10 19:16:59 +0400

    /idk

---
+++ frontend/src/components/middleware/reduxLogger.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider, useSelector,useDispatch } from "react-redux";
+
+
+const icon = useSelector(state = >state.icon);
+const dispatch = use dispatch()
