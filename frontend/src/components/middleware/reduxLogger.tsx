12d04fb1d8e64adf9edbca0a8330f4a00b1ba630
PARADOXnews <124525161+PARADOXnews@users.noreply.github.com>
2025-09-10 20:49:38 +0400

    Update reduxLogger.tsx

--- frontend/src/components/middleware/reduxLogger.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider, useSelector,useDispatch } from "react-redux";
-
-
-const icon = useSelector(state = >state.icon);
-const dispatch = use dispatch()
