cfa9ae1cea674fd1b4ea4f13f28c312a091bc79c
PARADOXnews <124525161+PARADOXnews@users.noreply.github.com>
2025-09-13 19:13:40 +0400

    fix/album card

--- frontend/src/app/(site)/album-page/page.tsx
+++ frontend/src/app/(site)/album-page/page.tsx
@@ -2,18 +2,48 @@
 import NewsComponent from "@/components/NewsComponent/NewsComponent";
 import styles from "./page.module.scss";
 import SongListTable from "@/components/SongListTable/SongListTable";
+import { useEffect, useState } from "react";
+import AlbumCard from "@/components/AlbumCard/AlbumCard";
+import photo from "../../../assets/images/table/albumphoto.png";
+import "@/../styles/defaults/defaultGrid.scss";
+import { usePathname } from "next/navigation";
+import { useActiveTab } from "@/components/Context/ActiveTabContext";
+
 
 export default function AlbumPage() {
+  const {activeTab, setActiveTab} = useActiveTab();
+  const pathname = usePathname();
+
+
+  useEffect(() => {
+    setActiveTab(1);
+  }, [pathname, setActiveTab]);
+
   return (
+
+
+
     <main className={styles.main}>
-      <NewsComponent
-        plays={"Released 07/12/2023"}
-        title="Seek For Marktoop"
-        imageUrl="/Images/NewsComponent/NewComponentTest.jpg"
-      />
-      <div>
-        <SongListTable />
-      </div>
-    </main>
+
+      {activeTab === 1 && 
+      <div className="albumCard">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <AlbumCard key={i} onClick={() =>setActiveTab(2)} title="album title" imageUrl={photo}></AlbumCard>
+        ))}
+      </div>}
+
+
+      {activeTab === 2 &&
+        <>
+          <NewsComponent
+            plays={"Released 07/12/2023"}
+            title="Seek For Marktoop"
+            imageUrl="/Images/NewsComponent/NewComponentTest.jpg"
+          />
+          <div>
+            <SongListTable />
+          </div>
+        </>}
+    </main >
   );
 }
