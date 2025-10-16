b63c45b6742d3c44a6ca8303dceeba85b5e9a0a8
PARADOXnews <achirubashvili@gmail.com>
2025-10-16 13:00:22 +0400

    Update Albums.tsx

--- frontend/src/components/Authors/Albums.tsx
+++ frontend/src/components/Authors/Albums.tsx
@@ -7,9 +7,9 @@ import "@/../styles/Defaults/defaultGrid.scss";
   
 interface Author {
   id?: number;
-  name: string;
-  avatarFileName: string;
-  avatarUrl:string;
+  name?: string;
+  avatarFileName?: string;
+  coverUrl:string;
   cover?: string;
   title?: string;
   releaseDate?: string;
@@ -24,7 +24,7 @@ const Authors: React.FC = () => {
 
   useEffect(() => {
     axios
-      .get<Author[]>(`http://localhost:4000/albums/`)
+      .get<Author[]>(`https://frisson-music-app.s3.eu-north-1.amazonaws.com/albums`)
       .then((response) => {
         console.log("📦 Received from backend:", response.data);
         setAuthors(response.data);
@@ -52,7 +52,7 @@ const Authors: React.FC = () => {
           >
             {a.name && (
               <img
-                src={a.avatarUrl}
+                src={a.coverUrl}
                 alt={a.title || "cover"}
                 width="200"
                 style={{ borderRadius: "10px", marginBottom: "10px" }}
