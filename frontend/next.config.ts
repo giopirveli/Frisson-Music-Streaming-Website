779326a8ddd1ceee29219dc38deb4f9ec0050d25
PARADOXnews <achirubashvili@gmail.com>
2025-10-20 21:28:07 +0400

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,9 @@
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    config.resolve.alias['@styles'] = path.resolve(__dirname, 'src/styles');
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'src/styles');
     return config;
   },
 };
-
-module.exports = nextConfig;
