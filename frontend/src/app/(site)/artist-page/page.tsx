a9dc189ea6403a9384458de465ad485326cc1a8c
PARADOXnews <achirubashvili@gmail.com>
2025-10-28 22:37:29 +0400

    Update page.tsx

--- frontend/src/app/(site)/artist-page/page.tsx
+++ frontend/src/app/(site)/artist-page/page.tsx
@@ -2,10 +2,10 @@
 
 import NewsComponent from "@/components/NewsComponent/NewsComponent";
 import { useEffect, useState } from "react";
-import { Colors } from "../../../../styles/colors.enum";
+// import { Colors } from "../../../../styles/colors.enum";
 import styles from "./page.module.scss";
 import Table from "@/components/Table/Table";
-import AlbumCard from "@/components/AlbumCard/AlbumCard";
+// import AlbumCard from "@/components/AlbumCard/AlbumCard";
 import photo from "../../../assets/images/table/albumphoto.png";
 import { usePathname } from "next/navigation";
 import { useActiveTab } from "@/components/Context/ActiveTabContext";
@@ -37,7 +37,6 @@ export default function ArtistPage() {
       {activeTab === 2 && (
         <>
           <NewsComponent
-            color={Colors.White}
             title="peggy gou"
             button="follow"
             imageUrl="/Images/NewsComponent/peggyGou.jpg"
